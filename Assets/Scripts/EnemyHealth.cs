c45670f376446431b9c76ce4a5839563456fa91b
tus-hollyoleary-22 <117648912+tus-hollyoleary-22@users.noreply.github.com>
2024-02-20 09:17:40 +0000

    Fixed Damage FX

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,15 +7,26 @@ public class EnemyHealth : MonoBehaviour
     public float enemyHealth;
     public float maxHealth;
 
-    public 
+    public float fxStrength;
+    public float fxSpeed;
+
+    public Material damageFX;
 
     void Start()
     {
         maxHealth = enemyHealth;
+
+        damageFX.SetFloat("_glow_speed", 0f);
+        damageFX.SetFloat("_glow_strength", 0f);
     }
 
     public void TakeDamage(float damageAmount)
     {
+        damageFX.SetFloat("_glow_speed", 5f);
+        damageFX.SetFloat("_glow_strength", 0.06f);
+
+        StartCoroutine("DamageFlashTimer");
+
         enemyHealth -= damageAmount;
 
         if (enemyHealth <= 0)
@@ -26,7 +37,18 @@ public class EnemyHealth : MonoBehaviour
 
     public void Die()
     {
+        damageFX.SetFloat("_glow_speed", 0f);
+        damageFX.SetFloat("_glow_strength", 0f);
+
         GetComponent<DropLoot>().InstantiateLoot(transform.position);
         this.gameObject.SetActive(false);
+    }
+
+    public IEnumerator DamageFlashTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        damageFX.SetFloat("_glow_speed", 0f);
+        damageFX.SetFloat("_glow_strength", 0f);
     }
 }
