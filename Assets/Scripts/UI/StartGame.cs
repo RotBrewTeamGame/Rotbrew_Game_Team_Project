06a21f309ec5bc9d4fad34456b9ec1113144c606
Shane MacNamara <k00270852@student.tus.ie>
2024-04-08 10:48:08 +0100

    Edited StartGame Script

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -3,11 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public string GAD3_NightOwls_Rotbrew_V2;
+    public string Rotbrew_CityCentre;
 
     public void LoadSceneOnClick()
     {
         LightingManager.Instance.SaveLightingSettings(); // Save lighting settings before loading new scene
-        SceneManager.LoadScene(GAD3_NightOwls_Rotbrew_V2);
+        SceneManager.LoadScene(Rotbrew_CityCentre);
     }
 }
