fc8d55503401d9310ae80874e0072bd01d0fd2f8
Shane MacNamara <k00270852@student.tus.ie>
2024-04-09 11:11:30 +0100

    Crafting UI Fixed

--- Assets/Scripts/Crafting/EnableCrafting.cs
+++ Assets/Scripts/Crafting/EnableCrafting.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class EnableCrafting : MonoBehaviour
 {
     public GameObject craftingSystem;
+    public GameObject uI;
+    public bool triggered;
+    public bool interacted;
+    public FirstPersonMovement firstPersonMovement;
+    public FirstPersonLook firstPersonLook;
+    public Rigidbody rb;
 
-    private void Start()
+    void Start()
     {
         craftingSystem.SetActive(false);
+        uI.SetActive(false);
+        firstPersonMovement = GetComponent<FirstPersonMovement>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (triggered)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                interacted = !interacted;
+                craftingSystem.SetActive(interacted);
+                firstPersonMovement.enabled = !interacted;
+                firstPersonLook.enabled = !interacted;
+            }
+        }
+
+        if (interacted)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        }
+        else
+        {
+            rb.constraints &= ~RigidbodyConstraints.FreezePosition;
+
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "CraftingStation")
         {
-            craftingSystem.SetActive(true);
+            uI.SetActive(true);
+            triggered = true;
         }
     }
 
@@ -23,7 +57,9 @@ public class EnableCrafting : MonoBehaviour
     {
         if (collision.gameObject.tag == "CraftingStation")
         {
-            craftingSystem.SetActive(false);
+            uI.SetActive(false);
+            triggered = false;
+            craftingSystem?.SetActive(false);
         }
     }
 }
