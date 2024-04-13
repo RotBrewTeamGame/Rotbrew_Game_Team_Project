75a84b446dd07c3e7f7c864f2e644471d8106748
tus-hollyoleary-22 <K00268353@student.lit.ie>
2024-04-13 20:44:48 +0100

    Set up sewer shortcut script

--- Assets/Scripts/Events/SewerShortcut.cs
+++ Assets/Scripts/Events/SewerShortcut.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class SewerShortcut : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EnemyHealth bossHealth;
+    public GameObject sewerShortcut;
+    public Canvas interactionUI;
+    public bool sewerBossDead;
+
+    public void Start()
     {
-        
+        sewerBossDead = false;
+        sewerShortcut.SetActive(false);
+        interactionUI.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (bossHealth.enemyHealth == 0)
+        {
+            sewerBossDead = true;
+        }
+
+        if (sewerBossDead)
+        {
+            sewerShortcut.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (sewerBossDead)
+        {
+            interactionUI.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (sewerBossDead)
+        {
+            interactionUI.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneNavigator.instance.LoadCityCentre();
+        }
     }
 }
